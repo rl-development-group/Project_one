279e70c6830e934346df1bc64d011e8a187fe8e6
Bao Bill <741150372@qq.com>
2019-08-22 07:04:16 +1200

    Add files via upload

--- environment.py
+++ environment.py
@@ -13,11 +13,11 @@ in which the agents are run.
 
 class Environment:
     def __init__(self,graph,pre_rewards):
-        self.graphs = graph
+        self.graph = graph
         self.pre_rewards = pre_rewards
 
     def reset(self):
-        self.graph_init = self.graphs
+        self.graph_init = self.graph
         self.nodes = self.graph_init.nodes()
         #print (self.nodes)
         self.nbr_of_nodes = 0
@@ -32,49 +32,33 @@ class Environment:
         ego = []
         ego_2 = []
 
-        for n in nx.all_neighbors(self.graphs, node):
+        for n in nx.all_neighbors(self.graph, node):
             ego.append(n)
         print (ego)
-        for n in nx.generators.ego.ego_graph(self.graphs, node, 2):
+        for n in nx.generators.ego.ego_graph(self.graph, node, 2):
             ego_2.append(n)
         print (ego_2)
+        
         return ego_2
         #return self.observation
 
     def act(self,node, preferences):
         
-        self.observation[:,node,:]=1
+        #self.observation[:,node,:]=1
         reward = self.get_reward(self.observation, preferences, node)
         return reward
 
     def get_reward(self, observation, preferences, node):
         # utilities before
-        bridging_capital = self.graphs.bridging_capital()
-        #print ("****************** ",self.graphs.bonding_capital(node))
-        utility_after_acting = {}  # rewards for all nodes after acting
-        utility_before_acting = {}  # rewards for all nodes before acting
-        rewards = {} # rewards after acting
-        utility_before_acting[node] = preferences[node] * self.graphs.bonding_capital(node) + \
-                            (1 - preferences[node]) * bridging_capital[node]
+        bridging_capital = self.graph.bridging_capital()
+        #print ("****************** ",self.graph.bonding_capital(node))
         
-        utility_after_acting[node] = preferences[node] * self.graphs.bonding_capital(node) + \
+        utility_after_acting = preferences[node] * self.graph.bonding_capital(node) + \
                            (1 - preferences[node]) * bridging_capital[node]
-        rewards[node] = utility_after_acting[node] - utility_before_acting[node]
+        after_rewards = utility_after_acting - self.pre_rewards[node]
 
-        return (rewards[node])
+        return after_rewards
 
-    def update(self, node):
-        two_hop_neigh = []
-        
-        two_hop_neigh = self.graphs.two_hop_neighbors(node)
-        print(two_hop_neigh)
-        
-        #two_hop_neigh = two_hop_neigh.remove(node)
-        print (two_hop_neigh)
-        for i in two_hop_neigh:
-            if node != i:
-                print("node: ",node)
-                print("i: ",i)
-                self.graphs.add_edge(node,i)
-        
-        return self.graphs
+    def update(self, actions):
+        for i in range(0,100):
+            self.graph.add_edge(i, actions[i])
